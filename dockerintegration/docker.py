0fbe64c923687fca6e35b1481f7d0a2a15ce847a
Shane Drury <shane.drury@fanduel.com>
2016-01-25 12:36:03 +0000

    Initial DockerProject

---
+++ dockerintegration/docker.py
@@ -0,0 +1,3 @@
+class DockerProject(object):
+    def __init__(self, project_name):
+        self.project_name = project_name
--- dockerintegration/tests/test_docker.py
+++ dockerintegration/tests/test_docker.py
@@ -1,2 +1,7 @@
-def test_foo():
-    pass
+from dockerintegration.docker import DockerProject
+
+
+def test_project_name():
+    project_name = 'foo'
+    project = DockerProject(project_name=project_name)
+    assert project_name == project.project_name
