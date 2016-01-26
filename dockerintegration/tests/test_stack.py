dab2077e49256fa39fa02021061d94f7d58a22fd
Shane Drury <shane.drury@fanduel.com>
2016-01-26 15:26:50 +0000

    Basic tests for Stack

---
+++ dockerintegration/tests/test_stack.py
@@ -0,0 +1,26 @@
+from dockerintegration.docker import DockerClientStub
+from dockerintegration.stack import Stack
+
+
+def test_post_setup_state():
+    docker_client = DockerClientStub()
+    stack = Stack(docker_client)
+    stack.setup()
+    assert docker_client.state == docker_client.STATES['UP']
+    assert docker_client.created_state == docker_client.CREATED_STATES['CREATED']
+
+
+def test_post_teardown_state():
+    docker_client = DockerClientStub()
+    stack = Stack(docker_client)
+    stack.teardown()
+    assert docker_client.state == docker_client.STATES['STOPPED']
+    assert docker_client.created_state == docker_client.CREATED_STATES['NOT_CREATED']
+
+
+def test_post_teardown_state_no_remove():
+    docker_client = DockerClientStub()
+    stack = Stack(docker_client)
+    stack.teardown(remove=False)
+    assert docker_client.state == docker_client.STATES['STOPPED']
+    assert docker_client.created_state == docker_client.CREATED_STATES['CREATED']
