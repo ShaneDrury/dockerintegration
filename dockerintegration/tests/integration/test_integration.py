285c29bd1ef374dc56fe68fa457a4e0027a48b8d
Shane Drury <shane.drury@fanduel.com>
2016-05-29 12:08:05 +0100

    Test scaling services

--- dockerintegration/tests/integration/test_integration.py
+++ dockerintegration/tests/integration/test_integration.py
@@ -1,31 +1,42 @@
-import pytest
 import six
 
 
-@pytest.fixture
-def service_internal_ports():
-    return {
-        'oneport': [59001, ],
-        'twoports': [59002, 59003]
-    }
+SERVICES = {
+    'oneport': [59001, ],
+    'twoports': [59002, 59003]
+}
 
 
-def test_docker_fixture_services(docker_fixture, service_internal_ports):
-    for service_name, ports in six.iteritems(service_internal_ports):
+def test_docker_fixture_services(docker_fixture):
+    for service_name, ports in six.iteritems(SERVICES):
         service = docker_fixture.services[service_name]
         for container in service:
             docker_ports = container.addresses.keys()
             assert sorted(ports) == sorted(docker_ports)
 
 
-def test_docker_fixture_ports(docker_fixture, service_internal_ports):
+def test_docker_fixture_ports(docker_fixture):
     for service_name, port_mapping in six.iteritems(docker_fixture.ports):
         internal_ports = port_mapping.keys()
-        assert sorted(service_internal_ports[service_name]) == sorted(internal_ports)
+        assert sorted(SERVICES[service_name]) == sorted(internal_ports)
 
 
-def test_docker_get_first_container(docker_fixture, service_internal_ports):
-    for name, ports in six.iteritems(service_internal_ports):
+def test_docker_get_first_container(docker_fixture):
+    for name, ports in six.iteritems(SERVICES):
         for port in ports:
             address = docker_fixture.get_first_address_by_service(name, port)
             assert docker_fixture.ports[name][port][0] == address.port
+
+
+def test_docker_scale(docker_fixture):
+    client = docker_fixture.docker_client
+    assert len(docker_fixture.services['oneport']) == 1
+    for service in client.project.services:
+        if service.name == 'oneport':
+            service.scale(2)
+            break
+    else:
+        raise ValueError("No service 'oneport'")
+    assert len(docker_fixture.services['oneport']) == 2
+    service.scale(1)
+    assert len(docker_fixture.services['oneport']) == 1
